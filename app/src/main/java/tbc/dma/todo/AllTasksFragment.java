008b01837a7487e716a35c6d2a5d0feb1e7c97c8
Manoj Karki <karkimanoj2020@gmail.com>
2020-05-16 12:20:54 +0545

    AddEdit Task XML Finished

---
+++ app/src/main/java/tbc/dma/todo/AllTasksFragment.java
@@ -0,0 +1,28 @@
+package tbc.dma.todo;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AllTasksFragment extends Fragment {
+
+    public AllTasksFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+    }
+}
