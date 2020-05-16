399b9ab38844ae23c2ac5c88411e5a544f0e1439
Manoj Karki <karkimanoj2020@gmail.com>
2020-05-16 12:29:59 +0545

    Adding HighPriorityFragment

--- app/src/main/java/tbc/dma/todo/AllTasksFragment.java
+++ app/src/main/java/tbc/dma/todo/AllTasksFragment.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.view.ViewGroup;
 
 
+
 /**
  * A simple {@link Fragment} subclass.
  */
@@ -22,7 +23,8 @@ public class AllTasksFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_all_tasks, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_tasks, container, false);
+        return view;
     }
 }
